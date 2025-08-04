368d5d671d30557905b0db8568e36a7c64262255
Mirxa893 <mirxakamran893@gmail.com>
2025-08-04 13:55:11 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -138,7 +138,7 @@ export default async function RootLayout({
               color: effects.lines.color,
             }}
           />
-          <Flex fillWidth minHeight="16" hide="s"/>
+          <Flex fillWidth minHeight="16" className="hidden sm:flex"/>
             <Header />
             <Flex
               zIndex={0}
